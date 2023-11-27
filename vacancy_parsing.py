6331719702812a7255b11164349e58b90374a8de
Max Demm <maxymka21@gmail.com>
2023-11-27 21:37:15 +0200

    commit

--- vacancy_parsing.py
+++ vacancy_parsing.py
@@ -7,14 +7,16 @@ print(r.status_code)
 
 soup = bs(r.text, "html.parser")
 vacan_names = soup.find_all('h2', class_=True)
-for name in vacan_names:
+vacan_info = soup.find_all('p', class_="overflow text-muted add-top-sm cut-bottom")
+
+for i in range(len(vacan_names)):
+    name = vacan_names[i]
     if name.a and 'title' in name.a.attrs:
         print(name.a['title'])
         print('https://www.work.ua' + name.a['href'])
-    else:
-        print()
+        info = vacan_info[i]
+        print(info.text)
+        print("_" * 150)
 
-vacan_info = soup.find_all('p', class_="overflow text-muted add-top-sm cut-bottom")
-for info in vacan_info:
-    print(info.text)
+
 
